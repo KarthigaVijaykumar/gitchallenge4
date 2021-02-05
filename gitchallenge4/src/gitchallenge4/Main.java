34d88801c67615cb2bc5ae9be23a99e4a8951335
KarthigaVijaykumar <karthigamadhu8599@gmail.com>
2021-02-05 10:24:02 +0530

    Resolved merge conflict and merged

--- gitchallenge4/src/gitchallenge4/Main.java
+++ gitchallenge4/src/gitchallenge4/Main.java
@@ -5,11 +5,7 @@ public class Main {
 		int a = 5;
 		int b = 6;
 		System.out.println(a+b);
-<<<<<<< HEAD
-		System.out.println("challenge4");
-=======
 		System.out.println("made changes in main file");
 		System.out.println("karthiga");
->>>>>>> branch 'master' of https://github.com/KarthigaVijaykumar/gitchallenge4.git
 	}
 }

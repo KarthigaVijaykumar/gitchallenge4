8f10f00c23e1d7af220c9d1befe071ab4a7fc43d
KarthigaVijaykumar <karthigamadhu8599@gmail.com>
2021-02-08 11:53:23 +0530

    updated some lines

--- gitchallenge4/src/gitchallenge4/Main.java
+++ gitchallenge4/src/gitchallenge4/Main.java
@@ -9,5 +9,6 @@ public class Main {
 		System.out.println("karthiga");
 		System.out.println("MADE SOME CHANGES");
 		System.out.println("CREATING MERGE CONFLICT");
+		System.out.println("updating some changes");
 	}
 }
